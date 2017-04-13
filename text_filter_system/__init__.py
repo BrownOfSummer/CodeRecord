954c83885f735af9915aaeaedbe7bfd1ac8b71eb
BrownOfSummer <l846093185@163.com>
2017-04-13 17:05:01 +0800

    test_filte_system first push

---
+++ text_filter_system/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+from excute_training import train
+from sumit_training_data import submit
+from search_train_data import query
+from reset_train_result import reset
